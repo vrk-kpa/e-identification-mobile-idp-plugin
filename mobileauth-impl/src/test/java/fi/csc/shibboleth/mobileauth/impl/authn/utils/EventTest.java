00d4605553df0bb77785dc2d2bfd717f168541d2
Keijo Korte <keijo.korte@kvak.net>
2016-02-27 00:42:31 +0200

    Just testing unit tests

---
+++ mobileauth-impl/src/test/java/fi/csc/shibboleth/mobileauth/impl/authn/utils/EventTest.java
@@ -0,0 +1,29 @@
+package fi.csc.shibboleth.mobileauth.impl.authn.utils;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+
+import fi.csc.shibboleth.mobileauth.impl.authn.Events;
+import net.shibboleth.idp.authn.impl.PopulateAuthenticationContextTest;
+
+//TODO: Stupid extend. First that came to my mind 
+public class EventTest extends PopulateAuthenticationContextTest {
+
+    /** The action to be tested. */
+    private Events action;
+
+    /** {@inheritDoc} */
+    @BeforeMethod
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    /** Testing events */
+    // Just testing. Basically really stupid test
+    @SuppressWarnings("static-access")
+    public void testSuccess() {
+        Assert.assertNotEquals(action.success.event(this), toString(), action.failure.event(this).toString());
+
+    }
+
+}
